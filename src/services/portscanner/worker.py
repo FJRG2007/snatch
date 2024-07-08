c16f7af5be5cda0096d49d28ac44a25c3e649d68
FJRG2007 <fjrg2007@tpeoficial.com>
2024-07-08 14:03:35 +0200

    portscanner

--- src/services/portscanner/worker.py
+++ src/services/portscanner/worker.py
@@ -36,10 +36,10 @@ def main(target, ports, threadsNumber=10, saveonfile=False):
     # Add Banner.
     print(f"{cl.des_space}" + "-" * 50)
     print(f"{cl.des_space}Scanning Target: {target}")
-    print(f"{cl.des_space}Scanning ports: {'All (65535)' if ports == '*' else ports}")
+    print(f"{cl.des_space}Scanning ports: {'All (65535)' if ports == '*' else ports.capitalize()}")
     print(f"{cl.des_space}Scanning started at: {str(datetime.now())}")
     print(f"{cl.des_space}Number of threads: {threadsNumber}")
-    print(f"{cl.des_space}" + "-" * 50)
+    print(f"{cl.des_space}" + "-" * 50 + "\n")
     if not isinstance(threadsNumber, int) or threadsNumber < 1 or threadsNumber > 9999: return terminal("e", "Set a valid number of threads between 1 and 9999.")
     # Semaphore to limit the number of simultaneously active threads.
     semaphore = Semaphore(threadsNumber)
