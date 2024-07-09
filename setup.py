e1eaff3d0ab3a9b1b0aedcdb26eae9239d30cad7
FJRG2007 <fjrg2007@tpeoficial.com>
2024-07-09 11:57:11 +0200

    keywords

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="snatch",
-    version="0.1",
+    version="0",
     packages=find_packages(),
+    keywords=["artificial intelligence", "osint", "reconnaissance", "information gathering"],
     include_package_data=True,
     entry_points = {
         "console_scripts": [
