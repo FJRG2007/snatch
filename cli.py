af60db5633b1b8d7fd9a31544d3e88b9d40f3917
FJRG2007 <fjrg2007@tpeoficial.com>
2024-08-23 10:32:20 +0200

    issues

--- cli.py
+++ cli.py
@@ -1,10 +1,9 @@
 from dotenv import load_dotenv
-import sys, click, pyfiglet, importlib
-from src.lib.config import config
 from src.utils.snatch import Snatch
 from textual.widgets import Markdown
-from src.utils.basics import cls, terminal
 from src.lib import data, colors as cl
+import sys, click, pyfiglet, importlib
+from src.utils.basics import cls, terminal
 from textual.app import App, ComposeResult
 
 def get_function(module_name, function_name="main"):
@@ -15,7 +14,7 @@ def get_function(module_name, function_name="main"):
 def cli():
     cls()
     print(pyfiglet.figlet_format("SNATCH"))
-    print(f'\n{cl.des_space}{cl.b}>> {cl.w}Welcome to Snatch, remember to use it responsibly. \n{cl.des_space}{cl.b}>> {cl.w}Join to our Discord server on tpeoficial.com/dsc\n{cl.des_space}{cl.b}>> {cl.w}Version: {data.version}\n')
+    print(f'\n{cl.des_space}{cl.b}>> {cl.w}Welcome to Snatch, remember to use it responsibly. \n{cl.des_space}{cl.b}>> {cl.w}Join to our Discord server on tpe.li/dsc\n{cl.des_space}{cl.b}>> {cl.w}Version: {data.version}\n')
     if not sys.version[0] in "3": return terminal("e", "Snatch only works properly with Pytnon 3. Please upgrade/use Python 3.", exitScript=True)
     load_dotenv(override=True)
 
@@ -137,5 +136,4 @@ def main():
     except KeyboardInterrupt as e: terminal(KeyboardInterrupt)
     except Snatch.InvalidOption as e: terminal("iom")
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": main()
